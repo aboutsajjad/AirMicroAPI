95607870dae0d2d21c6b956b1801e9852e8a21d8
Sajjad Aboutalebi <sajjad.abo@gmail.com>
2019-09-09 18:12:12 +0430

    Base Model And Media

--- weather/models.py
+++ weather/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from utils.base_model import BaseModel
 
 
-class Weather(models.Model):
+class Weather(BaseModel):
     temperature = models.FloatField()
     pressure = models.FloatField()
     humidity = models.FloatField()
