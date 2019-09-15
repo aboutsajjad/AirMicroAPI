a1217398939a32af59826bea6e65567f9d23e222
Sajjad Aboutalebi <sajjad.abo@gmail.com>
2019-09-16 02:25:35 +0430

    meteocalc

--- weather/models.py
+++ weather/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.base_model import BaseModel
+from meteocalc import Temp, dew_point, heat_index
 
 
 class Weather(BaseModel):
@@ -8,3 +9,15 @@ class Weather(BaseModel):
     humidity = models.FloatField()
     altitude = models.FloatField()
     mics5524 = models.FloatField()
+
+    @property
+    def temp(self):
+        return Temp(self.temperature, 'C')
+
+    @property
+    def dew_point(self):
+        return dew_point(temperature=self.temp, humidity=self.humidity).c
+
+    @property
+    def heat_index(self):
+        return heat_index(temperature=self.temp, humidity=self.humidity).c
